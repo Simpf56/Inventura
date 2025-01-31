7e244b30afd528e27ac8d13f5da08abf4612e635
Simpf56 <xsorex561@gmail.com>
2025-01-31 18:36:35 +0100

    NvpCRouse- Gotov

---
+++ Backend/Inventura.sql
@@ -0,0 +1,76 @@
+﻿﻿use master;
+go
+
+drop database if exists Inventura;
+go
+
+create database Inventorija collate Croatian_CI_AS;;
+go
+
+use Inventorija;
+go
+
+create table Nabavljaci(
+	sifra int not null primary key identity (1,1),
+	ime varchar (100) not null,
+	prezime varchar (100) not null,
+	naziv varchar (100) not null,
+	kontakt varchar (100) not null,
+	br_tel varchar (100) not null
+);
+
+create table Proizvodi(
+	sifra int not null primary key identity (1,1),
+	naziv varchar (100)not null,
+	cijena decimal (22,2) not null,
+	nabavljac int foreign key references Nabavljaci(sifra)
+);
+
+create table Kupci(
+	sifra int not null primary key identity (1,1),
+	ime varchar (50)not null,
+	prezime varchar (50)not null,
+	br_tel varchar (50) not null,
+	adresa varchar (100), 
+	datum_rod date
+);
+
+create table Narudzbe(
+	sifra int not null primary key identity (1,1),
+	ukupan_iznos decimal (22,2) not null,
+	datum date,
+	[status] varchar (20),
+	kupac int foreign key references Kupci(sifra)
+);
+
+
+
+create table Stavke_narudzbe(
+	sifra int not null primary key  identity (1,1),
+	proizvod int foreign key references Proizvodi(sifra),
+	narudzba int foreign key references Narudzbe(sifra),
+	kolicina int not null,
+	cijena decimal (22,2)
+);
+
+insert into Nabavljaci ( naziv,ime,prezime,kontakt,br_tel)
+values	('Dobavljač d.o.o.','Ivan','Perić','ivan.peric@dobavljac.hr',0911234567),
+		('Global Supplies Ltd.','Petra','Novak','petra.novak@global.com',0917654321);
+
+insert into Proizvodi ( naziv,cijena)
+values	('T-Shirt',20.00),
+		('Laptop',4500.00),
+		('Kava',60.00);
+
+insert into Kupci (ime,prezime,br_tel,adresa,datum_rod)
+values	('Marko','Horvat',0912345678,'Ulica Marka 5, Zagreb','1990-05-15'),
+		('Ana','Kovačić',0918765432,'Ulica Ane 10, Split','1985-12-20');
+
+insert into Narudzbe (ukupan_iznos,datum,status,kupac)
+values	(100.00,'2024-11-22','Poslana',1),
+		(60.00,'2024-11-21','U tijeku',2);
+
+insert into Stavke_narudzbe (proizvod,narudzba,kolicina,cijena)
+values	(1,1,2,40.00),
+		(1,2,1,60.00),
+		(2,2,1,60.00);
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,17 +7,25 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.MapOpenApi();
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(o =>
+{
+    o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+});
 
 app.MapControllers();
 
