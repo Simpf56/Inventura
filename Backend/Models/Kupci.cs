6ee78fe18ae1bbf30df677303a073e1e67f7f8b3
Simpf56 <xsorex561@gmail.com>
2025-02-03 17:10:13 +0100

    ispravak

--- Backend/Models/Kupci.cs
+++ Backend/Models/Kupci.cs
@@ -1,6 +1,6 @@
 ﻿namespace Backend.Models
 {
-    public class Kupaci : Entitet
+    public class Kupci : Entitet
     {
         public string Ime { get; set; } = "";
         public string Prezime { get; set; } = "";
--- Backend/Models/Proizvodi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Models
-{
-    public class Proizvodi: Entitet
-    {
-        public string Naziv { get; set; }
-        public decimal Cijena { get; set; }
-        public int Nabavljaci { get; set; }
-    }
-}
